718bd446940fef2621433f47a6761889be913b83
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-10-21 19:55:32 +0300

    feat: add delete pizza button

--- internal/services/image.go
+++ internal/services/image.go
@@ -23,21 +23,13 @@ func NewImage(s3Client *s3.Client) Image {
 }
 
 func (srv *Image) UploadImage(ctx context.Context, file io.Reader) (string, error) {
-	// Create a buffer to store the file content
 	var buf bytes.Buffer
-
-	// Copy the file content into the buffer
-	size, err := io.Copy(&buf, file)
+	_, err := io.Copy(&buf, file)
 	if err != nil {
 		return "", err
 	}
 
-	fmt.Println(size)
-
-	// Generate a UUID for the image
 	id := uuid.New()
-
-	// Upload the image to S3 using the buffer's content
 	_, err = srv.s3Client.PutObject(ctx, &s3.PutObjectInput{
 		Bucket: aws.String(configs.Get().AWS.S3BucketName),
 		Key:    aws.String(getImageKey(id)),
@@ -47,7 +39,6 @@ func (srv *Image) UploadImage(ctx context.Context, file io.Reader) (string, erro
 		return "", err
 	}
 
-	// Return the image URL and its size
 	return fmt.Sprintf("%s/image/%s", configs.Get().GetBaseWebUrl(), getImageKey(id)), nil
 }
 
